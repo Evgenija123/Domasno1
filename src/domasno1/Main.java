13cd075a7fda66161c9c1a9337b179128891ca93
Evgenija123 <evgenija.divitarova1@gmail.com>
2021-10-28 00:02:51 +0200

    Initial commit

---
+++ src/domasno1/Main.java
@@ -0,0 +1,12 @@
+package domasno1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String pozdravuvanje="Hello World";
+        String fikt="OOP FIKT";
+        System.out.println("---------------------");
+	System.out.println(pozdravuvanje+" "+fikt);
+        System.out.println("---------------------");
+    }
+}
